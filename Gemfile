d2b607f83e378dd341477aea81bc33b421af48dc
Josh Teng <joshteng@me.com>
2015-07-10 14:01:13 +0000

    linkedin gem

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem "linkedin-oauth2", "~> 1.0"
 
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -35,6 +36,7 @@ gem "linkedin-oauth2", "~> 1.0"
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
+  gem 'figaro'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
--- config/initializers/linkedin.rb
+++ config/initializers/linkedin.rb
@@ -1,9 +1,9 @@
 LinkedIn.configure do |config|
-  config.client_id     = ENV["LINKEDIN_CLIENT_ID"]
-  config.client_secret = ENV["LINKEDIN_CLIENT_SECRET"]
+  config.client_id     = ENV["linkedin_id"]
+  config.client_secret = ENV["linkedin_secret"]
 
   # This must exactly match the redirect URI you set on your application's
   # settings page. If your redirect_uri is dynamic, pass it into
   # `auth_code_url` instead.
-  config.redirect_uri  = "https://getawesomeapp.io/linkedin/oauth2"
+  config.redirect_uri  = "http://ruby-100997.nitrousapp.com:3000/linkedin/oauth2"
 end
